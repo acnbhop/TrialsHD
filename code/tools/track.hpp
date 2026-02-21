43bfdf1a9fad997a4b73ba6b7ffe6916eabe4849
Grant Abernathy <abernathy.grant@icloud.com>
2026-02-21 16:33:31 -0600

    Init new `track.hpp`

---
+++ code/tools/track.hpp
@@ -0,0 +1,13 @@
+//
+// track.hpp: Tool for track (trk file) analysis and manipulation.
+//
+
+#pragma once
+
+#include "shared/util.hpp"
+
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <span>
+#include <cstring>
