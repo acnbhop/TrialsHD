b8d147f5c66f1c36fd628d0ab7eacea066259ee6
Grant Abernathy <abernathy.grant@icloud.com>
2026-02-21 16:19:55 -0600

    Rename to just `util.hpp`

---
+++ code/tools/util.hpp
@@ -0,0 +1,18 @@
+//
+// util.hpp
+//      Utility functions for Trials HD tools and research.
+//
+
+#pragma once
+
+
+
+#include <cstdint>
+#include <cstddef>
+#include <span>
+#include <stdexcept>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
