3cb6f5f77421ff32fc36c87d920a222c61b69325
Grant Abernathy <abernathy.grant@icloud.com>
2026-02-21 17:30:58 -0600

    `util.hpp` have limits

--- code/shared/util.hpp
+++ code/shared/util.hpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <limits>
 
 #if REDLYNX_POSIX
 #include <unistd.h>
@@ -29,6 +30,41 @@ using byte = std::byte;
 REDLYNX_NAMESPACE_BEGIN_TOOLS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #endif
+
+inline constexpr int8 int8_max = std::numeric_limits<int8>::max();
+inline constexpr int8 int8_min = std::numeric_limits<int8>::min();
+inline constexpr uint8 uint8_max = std::numeric_limits<uint8>::max();
+inline constexpr uint8 uint8_min = std::numeric_limits<uint8>::min();
+inline constexpr int16 int16_max = std::numeric_limits<int16>::max();
+inline constexpr int16 int16_min = std::numeric_limits<int16>::min();
+inline constexpr uint16 uint16_max = std::numeric_limits<uint16>::max();
+inline constexpr uint16 uint16_min = std::numeric_limits<uint16>::min();
+inline constexpr int32 int32_max = std::numeric_limits<int32>::max();
+inline constexpr int32 int32_min = std::numeric_limits<int32>::min();
+inline constexpr uint32 uint32_max = std::numeric_limits<uint32>::max();
+inline constexpr uint32 uint32_min = std::numeric_limits<uint32>::min();
+inline constexpr int64 int64_max = std::numeric_limits<int64>::max();
+inline constexpr int64 int64_min = std::numeric_limits<int64>::min();
+inline constexpr uint64 uint64_max = std::numeric_limits<uint64>::max();
+inline constexpr uint64 uint64_min = std::numeric_limits<uint64>::min();
+inline constexpr f32 f32_max = std::numeric_limits<f32>::max();
+inline constexpr f32 f32_min = std::numeric_limits<f32>::lowest();
+inline constexpr f64 f64_max = std::numeric_limits<f64>::max();
+inline constexpr f64 f64_min = std::numeric_limits<f64>::lowest();
+inline constexpr f32 f32_minimum = std::numeric_limits<f32>::min();
+inline constexpr f64 f64_minimum = std::numeric_limits<f64>::min();
+inline constexpr f32 f32_epsilon = std::numeric_limits<f32>::epsilon();
+inline constexpr f64 f64_epsilon = std::numeric_limits<f64>::epsilon();
+inline constexpr f32 f32_infinity = std::numeric_limits<f32>::infinity();
+inline constexpr f64 f64_infinity = std::numeric_limits<f64>::infinity();
+inline constexpr intmax intmax_max = std::numeric_limits<intmax>::max();
+inline constexpr intmax intmax_min = std::numeric_limits<intmax>::min();
+inline constexpr uintmax uintmax_max = std::numeric_limits<uintmax>::max();
+inline constexpr uintmax uintmax_min = std::numeric_limits<uintmax>::min();
+inline constexpr size size_max = std::numeric_limits<size>::max();
+inline constexpr size size_min = std::numeric_limits<size>::min();
+inline constexpr ptrdiff ptrdiff_max = std::numeric_limits<ptrdiff>::max();
+inline constexpr ptrdiff ptrdiff_min = std::numeric_limits<ptrdiff>::min();
 
 //
 // Converts a byte vector to a hex string.
@@ -128,7 +164,7 @@ inline f32 ReadBEFloat(std::span<const uint8> buffer, size offset)
 //
 // Writes a 32-bit big-endian integer to the given buffer at the given offset.
 //
-inline void WriteBE32(std::vector<uint8>& buffer, size offset, uint32 value)
+inline void WriteBE32(std::vector<uint8>& buffer, uint32 value)
 {
     buffer.push_back((value >> 24) & 0xFF);
     buffer.push_back((value >> 16) & 0xFF);
@@ -139,7 +175,7 @@ inline void WriteBE32(std::vector<uint8>& buffer, size offset, uint32 value)
 //
 // Writes a 16-bit big-endian integer to the given buffer at the given offset.
 //
-inline void WriteBE16(std::vector<uint8>& buffer, size offset, uint16 value)
+inline void WriteBE16(std::vector<uint8>& buffer, uint16 value)
 {
     buffer.push_back((value >> 8) & 0xFF);
     buffer.push_back(value & 0xFF);
@@ -152,7 +188,7 @@ inline void WriteBEFloat(std::vector<uint8>& buffer, f32 value)
 {
     uint32 be_int;
     std::memcpy(&be_int, &value, sizeof(be_int));
-    WriteBE32(buffer, buffer.size(), be_int);
+    WriteBE32(buffer, be_int);
 }
 
 #if 0
