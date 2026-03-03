f348853972675d527a1f889b8c0e29bffcf3eef6
Grant Abernathy <abernathy.grant@icloud.com>
2026-03-02 18:06:51 -0600

    Update `main.cpp`

--- code/game/main.cpp
+++ code/game/main.cpp
@@ -2,7 +2,32 @@
 // main.cpp
 //
 
-int main()
+#include "app.hpp"
+
+int main(int argc, char* argv[])
 {
-    return 0;
+	(void)argc;
+	(void)argv;
+
+	redlynx::game::Application ApplicationInstance;
+	redlynx::game::ApplicationConfiguration Configuration;
+	Configuration.Title = "Trials HD";
+	Configuration.Width = 1280;
+	Configuration.Height = 720;
+	Configuration.Resizable = true;
+	Configuration.Fullscreen = false;
+
+	if (!ApplicationInstance.Init(Configuration))
+	{
+		SDL_Log("[Main] Failed to initialize application.");
+		return EXIT_FAILURE;
+	}
+
+	ApplicationInstance.Run();
+
+	// Shutdown is called by the App deconstructor, but we are
+	// also going to make it explicit about it here.
+	ApplicationInstance.Shutdown();
+
+	return EXIT_SUCCESS;
 }
